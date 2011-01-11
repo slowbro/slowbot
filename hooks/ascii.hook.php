14bf039161ba2788bf9772f7b7a79532e3169a07
slowbro <slowbro@chatnets.net>
2011-01-11 16:43:44 -0700

    connect fix

--- hooks/ascii.hook.php
+++ hooks/ascii.hook.php
@@ -7,7 +7,7 @@ $time = $time[1] + $time[0];
 
 $start = $time;
 
-if(file_exists("ascii/".$this->buffex['4'].".txt")){
+if(file_exists("ascii/".@$this->buffex['4'].".txt")){
 	$file = file("ascii/".$this->buffex['4'].".txt");
 	foreach($file as $value){
 		$this->privmsg($channel, $value);
@@ -22,6 +22,14 @@ $finish = $time;
 
 $totaltime = ($finish - $start);
 //$this->privmsg($channel, "Time elapsed: $totaltime");
+} elseif (empty($this->buffex['4'])){
+    $d = opendir("ascii/");
+    $this->privmsg($channel, "ASCII LIST:");
+    while (false !== ($entry = readdir($d))) {
+        if(substr($entry, -4) == ".txt")
+            $this->privmsg($channel, str_replace(".txt", "", $entry));
+    }
+    closedir($d);
 } else {
 	$this->privmsg($channel, "Could not stat '".$this->buffex['4'].".txt'.");
 }
